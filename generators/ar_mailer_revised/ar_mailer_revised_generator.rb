430d95633b1b3375b0c751cedd4233cc71e4796e
Stefan Exner <stex@sterex.de>
2014-11-27 22:21:55 +0100

    Added Rails 2 Generator for model and initializer. Migration will not be covered as the sending process requires a Rails 4 application anyway.

---
+++ generators/ar_mailer_revised/ar_mailer_revised_generator.rb
@@ -0,0 +1,19 @@
+class ArMailerRevisedGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      model_name = (name || 'Email').classify
+
+      #Create the model
+      m.template 'model.rb', "app/models/#{model_name.downcase.underscore}.rb", :assigns => {:model_name => model_name}
+
+      #Create the initializer
+      m.template 'initializer.rb', 'config/initializers/ar_mailer_revised.rb', :assigns => {:model_name => model_name}
+    end
+  end
+
+  protected
+
+  def banner
+    "Usage: #{$0} ar_mailer_revised MODEL_NAME"
+  end
+end
