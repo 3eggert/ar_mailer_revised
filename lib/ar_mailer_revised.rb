40a873827783b2d43703c09c5de74b9fced69158
Thorsten Eggert <t.eggert@bbtel.de>
2018-05-15 16:17:45 +0200

    initialize config

--- lib/ar_mailer_revised.rb
+++ lib/ar_mailer_revised.rb
@@ -7,12 +7,16 @@ ActionMailer::Base.add_delivery_method :activerecord, ActionMailer::DeliveryMeth
 
 module ArMailerRevised
   @@config ||= OpenStruct.new({
-    :email_class => 'Email'
+    :email_class => 'Email',
+    :email_backup_class => 'EmailBackup',
+    :email_failed_class => 'FailedEmail'
   })
 
   def self.configuration(&proc)
     @@config ||= OpenStruct.new({
-      :email_class => 'Email'
+      :email_class => 'Email',
+      :email_backup_class => 'EmailBackup',
+      :email_failed_class => 'FailedEmail'
     })
     if block_given?
       yield @@config
@@ -36,5 +40,37 @@ module ArMailerRevised
   #
   def self.email_class_name
     @@config.email_class.classify
+  end
+
+  #
+  # @return [ActiveRecord::Base] (Email)
+  #   The class used to create new emails in the system
+  #
+  def self.email_failed_class
+    self.email_failed_class_name.constantize
+  end
+
+  #
+  # @return [String] (Email)
+  #   The email class' name
+  #
+  def self.email_failed_class
+    @@config.email_failed_class.classify
+  end
+
+  #
+  # @return [ActiveRecord::Base] (Email)
+  #   The class used to create new emails in the system
+  #
+  def self.email_backup_class
+    self.email_backup__class_name.constantize
+  end
+
+  #
+  # @return [String] (Email)
+  #   The email class' name
+  #
+  def self.email_backup_class
+    @@config.email_backup__class.classify
   end
 end
