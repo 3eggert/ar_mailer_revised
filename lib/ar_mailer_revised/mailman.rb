08b5d9a8dc3f93f11f26a1e843c1c991c355e24c
Thorsten Eggert <t.eggert@bbtel.de>
2020-06-15 13:58:13 +0200

    add logging and inc version

--- lib/ar_mailer_revised/mailman.rb
+++ lib/ar_mailer_revised/mailman.rb
@@ -332,6 +332,7 @@ module ArMailerRevised
       @lf = File.open('/tmp/ar_mailer.log', 'a')
       @lf.puts "Mailman--> Other error while connecting to '#{setting.host}:#{setting.port}'"
       @lf.puts "Mailman--> Complete Error (#{exception.class.to_s}): " + exception.to_s
+      @lf.close
       logger.warn "Other error while connecting to '#{setting.host}:#{setting.port}'"
       logger.warn "Complete Error (#{exception.class.to_s}): " + exception.to_s
       handle_custom_setting_removal(setting, emails)
